086405b1854edf1d8f52bf98aae24c9ffc537e19
Levi van der Maas <levi.vdmaas@gmail.com>
2024-08-27 00:00:09 +0200

    Ported kitty conf

--- modules/home-manager/terminal/kitty.nix
+++ modules/home-manager/terminal/kitty.nix
@@ -1,9 +1,20 @@
 {
-  programs.kitty.enable = true;
+  programs.kitty = {
+    enable = true;
 
-  programs.kitty.settings = {
-    background_opacity = "0.85";
-    enable_audio_bell = "";
-    background_blur = 1;
+    # font.name = "FiraCode Nerd Font";
+
+    settings = {
+      background_opacity = "0.80";
+      enable_audio_bell = false;
+      background_blur = 1;
+    };
+
+    keybindings = {
+      "ctrl+shift+space" = "launch --cwd=current --type=tab"; # New tab in same dir
+      "ctrl+shift+d" = "launch --cwd=current"; # New window in same dir
+      "ctrl+shift+n" = "new_os_window_with_cwd"; #New OS window in same dir
+    };
   };
 }
+
