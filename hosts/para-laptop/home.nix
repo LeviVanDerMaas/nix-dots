feefa03742ba36bb03bd475e0daa098c3b1e7fe2
Levi van der Maas <levi.vdmaas@gmail.com>
2024-09-21 14:36:33 +0200

    Bashrc open and launch fixes

--- hosts/para-laptop/home.nix
+++ hosts/para-laptop/home.nix
@@ -12,6 +12,10 @@
     ../../modules/home-manager/levi.nix
   ];
 
+  levi.extraPackages = with pkgs; [
+    blender
+  ];
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
