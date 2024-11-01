e86514e5324ac1ddda0b798637d2a57f4b973c5c
Levi van der Maas <levi.vdmaas@gmail.com>
2024-11-01 16:28:26 +0100

    Updated git formatting to make more sense

--- modules/home-manager/levi/git/default.nix
+++ modules/home-manager/levi/git/default.nix
@@ -1,5 +1,5 @@
 let
-  commit-fstr = "%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n          %C(white)%s%C(reset) %C(dim white)- %an (%ae)%C(reset)";
+  commit-fstr = "%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%w(0,10,10)%n%C(white)%s%C(reset) %C(dim white)- %an (%ae)%C(reset)";
 in
 {
   programs.git = {
@@ -11,8 +11,8 @@ in
       l = "log --graph --abbrev-commit --decorate --format=format:'${commit-fstr}'";
       graph = "log --graph --abbrev-commit --decorate --all --format=format:'${commit-fstr}'";
 
-      about = "show --stat --format=format:'${commit-fstr}%n          %b'";
-      shortabout = "show --shortstat --format=format:'${commit-fstr}%n          %b'";
+      about = "show --stat --format=format:'${commit-fstr}%n%b'";
+      shortabout = "show --shortstat --format=format:'${commit-fstr}%n%b'";
 
       a = "add";
       amend = "commit --amend";
