659484bf224246b85ee81160fa6cb077edff6627
Levi van der Maas <>
2024-11-03 16:25:24 +0100

    Enabled git lfs

--- modules/home-manager/levi/git/default.nix
+++ modules/home-manager/levi/git/default.nix
@@ -7,6 +7,7 @@ in
     userName = "Levi van der Maas";
     userEmail = "";
     difftastic.enable = true;
+    lfs.enable = true;
     aliases = {
       l = "log --graph --abbrev-commit --decorate --format=format:'${commit-fstr}'";
       graph = "log --graph --abbrev-commit --decorate --all --format=format:'${commit-fstr}'";
