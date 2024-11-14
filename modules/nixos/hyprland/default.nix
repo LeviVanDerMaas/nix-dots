ff0e4b0d42b47bb40274a2548ae8d4d28b108bff
Levi van der Maas <>
2024-11-14 10:52:06 +0100

    Moar Hypr stuff

--- modules/nixos/hyprland/default.nix
+++ modules/nixos/hyprland/default.nix
@@ -8,14 +8,28 @@ in
     enable = lib.mkEnableOption ''
       Hyprland system module. Use Home-Mamanger module for configuration.
     '';
+    installGTKPortal = lib.mkOption {
+      type = lib.types.bool;
+      default = true;
+      description = ''
+        Enables and makes available integration with the xdg-desktop-portal-gtk.
+        Hyprland implements its own portal but uses this one as fallback by default.
+        Notably, Hyprland's portal does not implement a file picker, and since Hyprland
+        is still in active early development it is not unlikely something may break at
+        some point, so having this as a fallback is useful.
+      '';
+    };
   };
 
   config = lib.mkIf cfg.enable {
     programs.hyprland.enable = true;
 
-    # I added these options once but I dont know why and it seems to
-    # no longer be necessary.
-    # xdg.portal.enable = true;
-    # xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+    # Hyprland has its own desktop portal that comes preinstalled with the
+    # nix module, however it will use the gtk portal by default as fallback
+    # when it cannot perform a certain task (most notably, Hyprlands portal
+    # does not implement a file picker).
+    xdg.portal.enable = cfg.installGTKPortal;
+    xdg.portal.extraPortals = 
+      lib.list.optional cfg.installGTKPortal [ pkgs.xdg-desktop-portal-gtk ];
   };
 }
