491127455d1d80e8cf7a9b8f91399de7dc48ec95
Levi van der Maas <levi.vdmaas@gmail.com>
2024-12-22 15:09:40 +0100

    Magically unfucks sddm theming again

--- modules/nixos/sddm/default.nix
+++ modules/nixos/sddm/default.nix
@@ -23,22 +23,30 @@ in
 
   config = lib.mkIf cfg.enable {
     services.xserver.displayManager.setupCommands = cfg.setupCommands;
-
-    qt.enable = true; # Needed to make custom themeing work
+    
     services.displayManager.sddm = {
       enable = true;
       # Default uses qt5 version, this package makes it use qt6 version.
       # When installing other desktop environments, they may also try to set this option and
       # cause conflicts even when packages match, so we force this option. This should not be
-      # a problem usually, but if enabling a DE does cause issues in SDDM this may be
+      # a problem usually, but if installing another DE does cause issues in SDDM this may be
       # the cause.
       package = lib.mkForce pkgs.kdePackages.sddm; 
 
       theme = "sddm-astronaut-theme"; # This theme requires qt6.
+      extraPackages = with pkgs; [
+        (sddm-astronaut.override {
+          themeConfig = {
+            background = "${../../../assets/wallpapers/woodrot.png}";
+            FullBlur = "false";
+            PartialBlur = "false";
+            OverrideTextFieldColor="#FFFFFF";
+            AccentColor="#CF0700";
+          };
+        })
+      ];
     };
 
-    # Settings this in sddm's extra packages does not work, so in
-    # the system environment it goes.
     environment.systemPackages = with pkgs; [
       (sddm-astronaut.override {
         themeConfig = {
