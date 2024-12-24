7a41f57ab4ed116c398439ffb2b9ee52518af8e6
Levi van der Maas <levi.vdmaas@gmail.com>
2024-12-24 02:29:57 +0100

    Moar progress

--- modules/home-manager/levi/default.nix
+++ modules/home-manager/levi/default.nix
@@ -38,6 +38,7 @@ in
     home.packages = with pkgs; [
       discord
       kdePackages.dolphin
+      xfce.thunar
       kdePackages.qtwayland
       kdePackages.qtsvg
       fastfetch
--- modules/home-manager/levi/hyprland/theming.nix
+++ modules/home-manager/levi/hyprland/theming.nix
@@ -6,14 +6,14 @@
   home.packages = with pkgs; [
     # Hyprland must use these for theming, as it is not a full DE and thus does not have
     # gtk or qt integration.
-    libsForQt5.qt5ct
-    kdePackages.qt6ct
-    kdePackages.breeze
-    kdePackages.breeze-gtk
-    kdePackages.breeze-icons
-    nwg-look
+    # libsForQt5.qt5ct
+    # kdePackages.qt6ct
+    # kdePackages.breeze
+    # kdePackages.breeze-gtk
+    # kdePackages.breeze-icons
+    # nwg-look
   ];
   wayland.windowManager.hyprland.settings.env = [
-    "QT_QPA_PLATFORMTHEME,qt5ct"
+    # "QT_QPA_PLATFORMTHEME,qt5ct"
   ];
 }
