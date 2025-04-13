3e6d049da50c01d0ef639a525dff5d688afe1f7f
Levi van der Maas <levi.vdmaas@gmail.com>
2025-04-13 15:59:44 +0200

    Stick gamescope to release 3.16.2 because 3.16.3 breaks --force-grab-cursor

--- modules/nixos/steam/default.nix
+++ modules/nixos/steam/default.nix
@@ -1,7 +1,17 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 let
   cfg = config.modules.nixos.steam;
+  gamescope-3_16_2 = (pkgs.gamescope.overrideAttrs (oldAttrs: {
+    version = "3.16.2";
+    src = pkgs.fetchFromGitHub {
+      owner = "ValveSoftware";
+      repo = "gamescope";
+      tag = "3.16.2";
+      fetchSubmodules = true;
+      hash = "sha256-vKl2wYAt051+1IaCGB1ylGa83WTS+neqZwtQ/4MyCck=";
+    };
+  }));
 in
 {
   options.modules.nixos.steam = {
@@ -29,12 +39,17 @@ in
           libkrb5
           keyutils
 
-          gamescope
+          gamescope-3_16_2
         ];
         extraLibraries = pkgs: with pkgs;
           lib.optionals cfg.install-gperftools [ gperftools ] ++ [
         ];
       };
+    };
+
+    programs.gamescope = {
+      enable = true;
+      package = gamescope-3_16_2;
     };
   };
 }
