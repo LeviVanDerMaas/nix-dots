3c38d655357c41154a2282523afdc7aeb1eabb20
Levi van der Maas <levi.vdmaas@gmail.com>
2025-06-20 22:25:39 +0200

    direnv progress

---
+++ homes/levi/cli/direnv.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
+    enableBashIntegration = true;
+  };
+}
+# To satisfy my own curiosity:
+# nix-direnv pulls off its symlinking build dependencies by adding an indirect root (symlink)
+# to a build of your shell/flake. An indirect symlink is a (poorly documented,
+# though pretty important) feature where you have a symlink that points out
+# of the store to another symlink that points to your built shell in the store.
+# When the garbage connecter checks liveness of store paths, for indirect roots it
+# checks if the symlink (outside the store) that the root points to still exists:
+# - If it does it then the store path that THAT symlink points to is considered live
+# - Otherwise it removes the indirect symlink from gcroots
+
+# These symlink SHOULD be stored under /nix/var/nix/gcroots/auto, from what I
+# can tell the source code of the garbage collector won't even properly
+# handle indirect symlinks when they are not under auto.
