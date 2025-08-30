203cba8b92486a2139dad2468eb3c3b76a8e2c0b
Levi van der Maas <levi.vdmaas@gmail.com>
2025-08-30 16:38:28 +0200

    Yeah still working baby

--- homes/levi/desktop/ags/widgets/applauncher/Applauncher.tsx
+++ homes/levi/desktop/ags/widgets/applauncher/Applauncher.tsx
@@ -3,18 +3,16 @@ import app from "ags/gtk4/app";
 import AstalApps from "gi://AstalApps";
 import Graphene from "gi://Graphene";
 import { createState, For } from "gnim";
-import { range } from "utils";
 
-const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor;
 
 type ApplauncherProps = {
-    listSize?: number;
-    appNameLength?: number;
+    maxNumApps: number;
+    maxNameLength?: number;
 };
 
 export default function Applauncher({
-    listSize = 9,
-    appNameLength: appNameMaxLength = 40
+    maxNumApps = 9,
+    maxNameLength = 40
 }: ApplauncherProps) {
     const apps = new AstalApps.Apps();
     const [appList, setAppList] = createState(new Array<AstalApps.Application>());
@@ -25,15 +23,14 @@ export default function Applauncher({
 
     function search(text: string) {
         if (text === "") {
-            print("Wiped app list")
             setAppList([]);
         } else {
-            setAppList(apps.fuzzy_query(text).slice(0, listSize));
+            const qResults = apps.fuzzy_query(text);
+            setAppList(qResults.slice(0, Math.min(maxNumApps, qResults.length)));
         }
     }
 
     function launch(app?: AstalApps.Application) {
-        print("Launch fired")
         win.hide()
         if (app) {
             return app.launch();
@@ -49,8 +46,6 @@ export default function Applauncher({
         if (mod === Gdk.ModifierType.ALT_MASK) {
             for (let i = 1; i < 10; i++) {
                 if (key === Gdk[`KEY_${i}` as keyof typeof Gdk]) {
-                    win.hide();
-                    print(appList.get().length)
                     launch(appList.get()[i - 1]);
                     return;
                 }
@@ -58,12 +53,12 @@ export default function Applauncher({
         }
     }
 
-    function clickreleaseHandler(_g: Gtk.GestureClick, _n: number, x: number, y: number) {
+    function clickpressHandler(_g: Gtk.GestureClick, _n: number, x: number, y: number) {
         const [, rect] = contentBox.compute_bounds(win);
         const position = new Graphene.Point({ x, y });
 
         if (!rect.contains_point(position)) {
-          win.visible = false;
+          win.hide();
           return true;
         }
     }
@@ -76,41 +71,53 @@ export default function Applauncher({
         }
     }
 
+    const { TOP, BOTTOM, LEFT, RIGHT } = Astal.WindowAnchor;
+    const { START, CENTER, END } = Gtk.Align;
+    const { VERTICAL, HORIZONTAL } = Gtk.Orientation;
     return <window
         application={app}
         name="applauncher"
+        anchor={TOP | BOTTOM | LEFT | RIGHT}
         exclusivity={Astal.Exclusivity.IGNORE}
         keymode={Astal.Keymode.EXCLUSIVE}
-        valign={Gtk.Align.CENTER}
-        halign={Gtk.Align.CENTER}
         $={(self) => win = self}
         onNotifyVisible={notifyVisibleHandler}
     >
         <Gtk.EventControllerKey onKeyPressed={keypressHandler} />
-        <Gtk.GestureClick onReleased={clickreleaseHandler} />
+        <Gtk.GestureClick onPressed={clickpressHandler} />
         <box
             $={(self) => (contentBox = self)}
-            orientation={Gtk.Orientation.VERTICAL}
+            orientation={VERTICAL}
+            valign={CENTER}
+            halign={CENTER}
+            heightRequest={800}
         >
             <entry 
                 $={(self) => searchEntry = self}
                 onNotifyText={({text}) => search(text)}
+                valign={START}
             />
-            <Gtk.Separator visible={appList((l) => l.length > 0)} />
-            <box orientation={Gtk.Orientation.VERTICAL}>
-                <For each={appList}>
-                    {(app, index) => <button onClicked={() => launch(app)}>
-                        <box orientation={Gtk.Orientation.HORIZONTAL}>
-                            <image iconName={app.iconName} />
-                            <label label={app.name} maxWidthChars={appNameMaxLength} wrap />
-                            <label label={index((i) => `Alt+${i + 1}`)} 
-                                halign={Gtk.Align.END}
-                                hexpand 
-                            />
-                        </box>
-                    </button>}
-                </For>
-            </box>
+            <scrolledwindow 
+                propagateNaturalHeight={true}
+                maxContentHeight={500}
+                hscrollbarPolicy={Gtk.PolicyType.NEVER}
+                valign={CENTER}
+            >
+                <box orientation={VERTICAL}>
+                    <For each={appList}>
+                        {(app, index) => <button onClicked={() => launch(app)}>
+                            <box orientation={HORIZONTAL}>
+                                <image iconName={app.iconName} />
+                                <label label={app.name} maxWidthChars={maxNameLength} wrap />
+                                <label label={index((i) => `Alt+${i + 1}`)} 
+                                    halign={END}
+                                    hexpand 
+                                />
+                            </box>
+                        </button>}
+                    </For>
+                </box>
+            </scrolledwindow>
         </box>
     </window>
 }
