de54c5df7f3c844a3fb1da5ba06c3281e6507433
Levi van der Maas <levi.vdmaas@gmail.com>
2025-11-28 17:49:53 +0100

    Added neovim plugin management summary

--- homes/levi/tui/neovim/default.nix
+++ homes/levi/tui/neovim/default.nix
@@ -33,6 +33,47 @@
       pyright
     ];
 
+    # The gist of how (neo)vim configuration, particularly for plugins, works (assuming no flags or special env vars):
+    # - On startup, look for an `init.{vim,lua}` file and run it. `:h init.lua`.
+    # - Beyond that, any script that adds or configures neovim features and any data that is
+    #   to be "provided" to neovim features is stored in directories under `'runtimepath'` and
+    #   packages under`'packpath'` (but this defaults to `'runtimepath'`). When the manual mentions
+    #   nvim using certain dirs, those dirs are almost always found through this. `:h 'runtimepath'`
+    # - Of particular interest for user configuration and plugins are the `autoload`, `lua` and
+    #   `plugin` directories.
+    #     - `autoload`:  define vimscript functions (and variables) lazily; these are not ran on
+    #       startup, but if you call a function or variable that doesn't exist but is defined here, it
+    #       will be automatically defined as given here. `:h autoload`
+    #     - `lua`: the neovim-api equivalent for autoload. Any lua script inside here can be called
+    #       using `require(name)`. `:h lua-module-load`. You may use either `.` or `.` as a directory
+    #       seperator, you can also call a directory if it contains an `init.lua`.
+    #     - `plugin`: Any lua or vimscript file underneath here is automatically executed upon
+    #       start; directories inside here are traversed recursively.
+    # - A "package" is *essentially* just a collection of directories that each contain
+    #   `'runtimepath'`-directories. The idea is that plugins are placed into a package so that they
+    #   do not have to concern themselves with their files conflicting with others, among other
+    #   benefits.
+    #     - Packages are stored inside a `pack` directory and this directory is searched for under
+    #       `packpath`.
+    #     - `pack/*/start/`: these directories are searched after (neo)vim is unable to find
+    #       a runtime-directory under `runtimepath`. Aditionally, the files in any `plugin`
+    #       directories under here are executed on startup (recursively).
+    #     - `~pack/*/opt/`: neovim does not search any runtime-directories under here nor execute
+    #       anything from here by default. However, any packages underneath here can be "loaded"
+    #       using `:packadd`. This adds the package to `runtimepath` and automatically runs its
+    #       `plugin` directory.
+    #     - As you can image managing plugins like this manually sucks, so typically people use
+    #       plugin managers (but most still use this system under the hood).
+    #     - Modern neovim plugins *should* have a lightweight `plugin` directory that only does
+    #     strictly necessary setup and put everything else in the other runtime-directories so that users can "transparently"
+    #       lazy-load the plugin in their config. Unfortuantely not every plugin does that. `:h
+    #       lua-plugin-lazy`.
+
+    # It is not immedieatly clear from the vim manual, but "packages" are 
+    # *essentially* just 
+    # The gist of how plugins work:
+    # - They are just ordinary scripts which execute in the context of nvim
+    # - Like 
     plugins = with pkgs.vimPlugins; [
       # Completion
       {
