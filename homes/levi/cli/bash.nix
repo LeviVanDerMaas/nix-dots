eb18472c5064a1cb32a6a99ad4fa97328f8b8dd0
Levi van der Maas <levi.vdmaas@gmail.com>
2025-12-03 16:46:27 +0100

    Some extra bashrc stuff

--- homes/levi/cli/bash.nix
+++ homes/levi/cli/bash.nix
@@ -13,11 +13,24 @@
       dots-dryb = "${rebuild} dry-build";
       dots-drya = "${rebuild} dry-activate";
 
-      ls = "ls --color=tty";
+      ls = "ls --color=auto --hyperlink=auto";
       l = "ls -Ah";
       ll = "ls -Ahl";
 
       zzz = "systemctl suspend";
     };
+
+    # There is both initExtra and bashrcExtra. They do the same except
+    # initExtra places its stuff after an interactive shell guard while
+    # bashrcExtra runs uncoditionally. Eventhough .bashrc is usually
+    # not automtically sourced by non-interave shells, there are some weird
+    # edge cases where it does like remote shells.
+    initExtra = ''
+      nixpkgs=${pkgs.path}
+
+      whichr() { realpath $(which $@); }
+      whichd() { dirname $(which $@); }
+      whichrd() { dirname $(realpath $(which $@)); }
+    '';
   };
 }
