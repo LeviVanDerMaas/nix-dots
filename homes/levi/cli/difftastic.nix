ad57662550e9c5e214b2c85ddfc979d410310a26
Levi van der Maas <levi.vdmaas@gmail.com>
2025-12-19 01:26:40 +0100

    Some changes to git config

--- homes/levi/cli/difftastic.nix
+++ homes/levi/cli/difftastic.nix
@@ -3,10 +3,12 @@
 {
   programs.difftastic = {
     enable = true;
-    git = {
-      enable = true;
-      diffToolMode = true;
-    };
+    # Note that enabling difftastic as the default git diffvierwer like this
+    # is gonna mess with things like fugitive.vim and telescope.nvim
+    # git = {
+    #   enable = true;
+    #   diffToolMode = true;
+    # };
   };
 
   programs.bash.shellAliases.diff = "${config.programs.difftastic.package}/bin/difft";
--- homes/levi/cli/git.nix
+++ homes/levi/cli/git.nix
@@ -1,5 +1,16 @@
+{ lib, config, ... }:
+
 let
-  commit-fstr = "%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%w(0,10,10)%n%C(white)%s%C(reset) %C(dim white)- %an (%ae)%C(reset)";
+  # Modify git aliases so that they use difft as a difftool
+  aliasesWithDifft = lib.mapAttrs (
+    let
+      strIfDifft = lib.optionalString config.programs.difftastic.enable;
+    in
+    n: v:
+      strIfDifft "-c diff.external=difft "
+      + v
+      + strIfDifft " --ext-diff"
+  );
 in
 {
   programs.git = {
@@ -10,22 +21,28 @@ in
       user.useConfigOnly = true;
       advice.detachedHead = false;
 
+      pretty.custom = "format:%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%w(0,10,10)%n%C(white)%s%C(reset) %C(dim white)- %an (%ae)%C(reset)%n%C(italic white)%b%C(reset)";
       alias = {
-        l = "log --graph --abbrev-commit --decorate --format=format:'${commit-fstr}'";
-        graph = "log --graph --abbrev-commit --decorate --all --format=format:'${commit-fstr}'";
-
-        about = "show --stat --format=format:'${commit-fstr}%n%b'";
-        shortabout = "show --shortstat --format=format:'${commit-fstr}%n%b'";
-
         a = "add";
         c = "commit";
-        d = "diff";
-        ds = "diff --staged";
         s = "status";
 
         amend = "commit --amend";
         softmerge = "merge --no-ff --no-commit";
         unstage = "restore --staged";
+
+        setUserPersonal = "!git config user.name ='Levi van der Maas'; git config user.email ='levi.vdmaas@gmail.com'";
+        setUserUni = "!git config user.name ='Levi van der Maas'; git config user.email ='l.a.vandermaas@student.tudelft.nl'";
+      }
+      // aliasesWithDifft {
+        l = "log --graph --abbrev-commit --decorate --pretty=custom";
+        graph = "log --graph --abbrev-commit --decorate --all --pretty=custom";
+
+        about = "show --stat --pretty=custom";
+        shortabout = "show --shortstat --pretty=custom";
+
+        d = "diff";
+        ds = "diff --staged";
       };
     };
   };
